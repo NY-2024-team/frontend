c0be3b02232840c82562ba2dde2dcec4a286b3de
Avenue Work <avenueus@mail.ru>
2023-12-15 17:23:40 +0300

    test

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
 	import type { PageData, ActionData } from './$types';
 
 	export let data: PageData;
 
 	export let form: ActionData;
 	$: console.log({data, form})
+
+    onMount(() => {
+    })
 </script>
 
 <form method="POST" action="?/login" use:enhance>
